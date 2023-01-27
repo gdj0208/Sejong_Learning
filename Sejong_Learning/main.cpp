22ebabfdd0cbf62bfa8f054af5fb4ff36985872c
gdj0208 <gdjsun@naver.com>
2023-01-27 14:07:47 +0900

    Initial Commit

---
+++ Sejong_Learning/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sejong_Learning
+//
+//  Created by 구동준 on 2023/01/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
